4797795b2f1bfb3573686b38d0d9592b30967c56
h5law <53987565+h5law@users.noreply.github.com>
2023-12-27 18:15:09 +0000

    chore: add interfaces to global interfaces file

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,7 @@
 package kvwal
 
+import "context"
+
 type (
 	// Key is a type alias for a byte slice representing a key in the store
 	Key []byte
@@ -77,4 +79,17 @@ type KVStore interface {
 	Clone() KVStore
 	// Equal returns true if the store is equal to the provided store
 	Equal(store KVStore) (bool, error)
+}
+
+// Entry is a type that represents a single entry in the log.
+type Entry []byte
+
+// WAL is an interface that represents a write-ahead-log.
+type WAL interface {
+	// Start the Write Ahead Logger, it takes a cancellable context.
+	Start(context.Context) error
+	// Write the give entry to the log.
+	Write(Entry) error
+	// Close the Write Ahead Logger and cancels the context.
+	Close() error
 }
