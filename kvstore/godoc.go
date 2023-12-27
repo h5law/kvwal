6de30cd9c717323324ef3013a7f9b4312a3016bf
h5law <53987565+h5law@users.noreply.github.com>
2023-12-27 18:15:43 +0000

    chore: fix grammar

--- kvstore/godoc.go
+++ kvstore/godoc.go
@@ -1,6 +1,6 @@
 // Package kvstore provides a simple key-value store safe for concurrent reads
-// and writes and and interface for interacting with the store.
-// The KVStore uses []byte for both keys and values, aliased as the Key and Value
+// and writes and interface for interacting with the store.
+// KVStore uses []byte for both keys and values, aliased as the Key and Value
 // types. This allows for the KVStore to be used with any type that can be
 // serialised or converted into a byte slice.
 package kvstore
